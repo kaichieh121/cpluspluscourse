8ab8e8ea3c56c0e44e206d6427403f9740e4c1d0
Kai Chieh Chang <jchang@PCNICHANGJEF01.attlocal.net>
2024-06-14 17:07:59 -0500

    commenting stuff out

--- exercises/basicTypes/basicTypes.cpp
+++ exercises/basicTypes/basicTypes.cpp
@@ -19,54 +19,54 @@ int main() {
   print(0u - 1u);       // FIXME
   print(1.0000000001f); // FIXME Why is this number not represented correctly?
 
-  std::cout << "\nUsing increment and decrement operators:\n";
-  int a = 1;
-  int b;
-  int c;
-  print(b = a++);       // Q: What is the difference between a++ and ++a?
-  print(c = ++a);
-  print(a);
-  print(b);
-  print(c);
+  // std::cout << "\nUsing increment and decrement operators:\n";
+  // int a = 1;
+  // int b;
+  // int c;
+  // print(b = a++);       // Q: What is the difference between a++ and ++a?
+  // print(c = ++a);
+  // print(a);
+  // print(b);
+  // print(c);
 
-  std::cout << "\nCompound assignment operators:\n";
-  int n = 1;
-  print(n *= 2);        // Q: Is there a difference between this and the next line?
-  print(n *= 2.9);
-  print(n -= 1.1f);
-  print(n /= 4);        // Q: Based on the results of these expressions, is there a better type to be used for n?
+  // std::cout << "\nCompound assignment operators:\n";
+  // int n = 1;
+  // print(n *= 2);        // Q: Is there a difference between this and the next line?
+  // print(n *= 2.9);
+  // print(n -= 1.1f);
+  // print(n /= 4);        // Q: Based on the results of these expressions, is there a better type to be used for n?
 
-  std::cout << "\nLogic expressions:\n";
-  const bool alwaysTrue = true;
-  bool condition1 = false;
-  bool condition2 = true;
-  print( alwaysTrue && condition1 && condition2 );
-  print( alwaysTrue || condition1 && condition2 );  // Q: Why does operator precedence render this expression useless?
-  print( alwaysTrue && condition1 || condition2 );
-  print(condition1 != condition1);                  // Q: What is the difference between this and the following expression?
-  print(!condition2);
+  // std::cout << "\nLogic expressions:\n";
+  // const bool alwaysTrue = true;
+  // bool condition1 = false;
+  // bool condition2 = true;
+  // print( alwaysTrue && condition1 && condition2 );
+  // print( alwaysTrue || condition1 && condition2 );  // Q: Why does operator precedence render this expression useless?
+  // print( alwaysTrue && condition1 || condition2 );
+  // print(condition1 != condition1);                  // Q: What is the difference between this and the following expression?
+  // print(!condition2);
 
-  std::cout << '\n';
-  print( false || 0b1 );     // Q: What is the difference between || and | ?
-  print( true | 0b10 );
-  printBinary( 0b1 & 0b10 );
-  printBinary( 0b1 | 0b10 );
-  printBinary( 0b1 && 0b10 ); // Q: Are the operators && and || appropriate for integer types?
-  printBinary( 0b1 || 0b10 );
+  // std::cout << '\n';
+  // print( false || 0b1 );     // Q: What is the difference between || and | ?
+  // print( true | 0b10 );
+  // printBinary( 0b1 & 0b10 );
+  // printBinary( 0b1 | 0b10 );
+  // printBinary( 0b1 && 0b10 ); // Q: Are the operators && and || appropriate for integer types?
+  // printBinary( 0b1 || 0b10 );
 
-  std::cout << "\nPlay with characters and strings:\n";
-  print("a");                 // Q: Why is this expression two bytes at run time, the next only one?
-  print('a');
+  // std::cout << "\nPlay with characters and strings:\n";
+  // print("a");                 // Q: Why is this expression two bytes at run time, the next only one?
+  // print('a');
 
-  char charArray[20];
-  char* charPtr = charArray;
-  charArray[19] = 0; // Make sure that our string is terminated with the null byte
+  // char charArray[20];
+  // char* charPtr = charArray;
+  // charArray[19] = 0; // Make sure that our string is terminated with the null byte
 
-  print(charArray);
-  print(charArray[0] = 'a');
-  print(charArray);
-  print(charArray[1] = 98);
-  print(charArray);
-  print(charPtr);
-  // FIXME: Ensure that no unexpected garbage is printed above
+  // print(charArray);
+  // print(charArray[0] = 'a');
+  // print(charArray);
+  // print(charArray[1] = 98);
+  // print(charArray);
+  // print(charPtr);
+  // // FIXME: Ensure that no unexpected garbage is printed above
 }
