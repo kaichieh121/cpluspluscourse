c963248a8a7000fdd79f10c1983fd913f79f10ef
Kai Chieh Chang <jchang@Kais-MBP-5.attlocal.net>
2024-06-22 09:51:29 -0500

    ptr exercise

---
+++ exercises/ptr_exercise/main.cpp
@@ -0,0 +1,45 @@
+#include "ptr_exercise.hpp"
+#include <sstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+  /* 
+  Exercise 1
+  Create a pointer that points to an integer x=5. Print the address and the value.
+  */
+
+
+  /*
+  Exercise 2
+  Create an array of int {1, 2, 3, 4, 5} and access it in 2 ways
+  */
+
+  /*
+  Exercise 3
+  What will be the output of following code
+  */
+  // int num[5]; 
+  // int* p; 
+  // p = num; 
+  // *p = 10; 
+  // p++; 
+  // *p = 20; 
+  // p = &num[2]; 
+  // *p = 30; 
+  // p = num + 3; 
+  // *p = 40; 
+  // p = num; 
+  // *(p + 4) = 50; 
+  // for (int i = 0; i < 5; i++) 
+  //     cout << num[i] << ", "; 
+
+  /*
+  Exercise 4
+  Repeat exercise 1~2 on heap
+  */
+
+
+  return 0;
+}
---
+++ exercises/ptr_exercise/ptr_exercise.cpp
@@ -0,0 +1,8 @@
+#include "ptr_exercise.hpp"
+
+#include <iostream>
+#include <array>
+#include <string>
+
+using namespace std;
+
