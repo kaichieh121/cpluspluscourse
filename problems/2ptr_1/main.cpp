5fa3ff67921cdfca0a1e7349b88531cea44dcc18
Kai Chieh Chang <jchang@Kais-MacBook-Pro-5.local>
2024-07-14 15:01:38 -0500

    ptr techniques

---
+++ problems/2ptr_1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    /*
+    1 ptr naive:
+    find max in A without using support functions
+    Time Complexity: O(N)
+    */
+    vector<int> A{35, 50, 20, 10, 75, 80};
+
+
+    /*
+    2 ptr techinque:
+    Given a sorted array B, having N integers, find if there exists any pair of elements (A[i], A[j]) such that their sum is equal to X.
+    Time Complexity: O(N)
+    */
+    vector<int> B{10, 20, 35, 50, 75, 80};
+    int X = 70;
+
+
+
+}
