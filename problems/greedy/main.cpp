e534be0585515091530805249ffacd9848f8d704
Kai Chieh Chang <jchang@Kais-MacBook-Pro-5.local>
2024-07-20 15:23:26 -0500

    iterator and greedy

---
+++ problems/greedy/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    /* 
+    Q1. Given a student has T time to study for algorithms, 
+        And the vector algorithm indicates the amount of time to learn each algo
+        What is the maximum number of algorithm he/she can learn?
+    */
+    vector<int> algorithm{4, 3, 8, 4, 7, 3};
+    int T = 15;
+
+    /*
+    Q2. vector coins describe the types of coins in a country
+        we want to find the minimum number of coins to make up to value V
+        for example, coins{2, 3, 1} and V=5, the result would be 2 using coin 2 and 3
+
+    */
+    vector<int> coins{1, 3, 4};
+    int V = 6;
+
+
+}
---
+++ problems/iterator/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    vector<int> v{1, 2, 3, 4, 5};
+
+    // 1. use standard method to iterate through the vector
+
+    // 2. use iterator to iterate through the vector
+
+    // 3. use auto to make your life easier
+
+    // 4. use elem : vector to make it even easier
+
+    // 5. use v.cbegin() and try to modify the element
+
+}
