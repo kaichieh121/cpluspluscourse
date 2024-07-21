29610ed406286d99d773b208235980597ae0c969
Kai Chieh Chang <jchang@Kais-MBP-5.attlocal.net>
2024-07-21 15:09:46 -0500

    pair tuple set

---
+++ problems/pair/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    // make a pair of string and int
+    // print out the member variables
+    // make another pair using makepair()
+    // swap the two pairs
+
+    // do the same for tuple
+
+    // Let's practice set stl
+    // make a set
+    // Add numbers (duplicates and unique)
+    // Remove a number
+    // Check if a number exists
+    // Display all numbers
+
+}
