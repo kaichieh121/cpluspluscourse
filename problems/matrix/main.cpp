cf5350cb637963073e3f789c28e6f2b24a9ec774
Kai Chieh Chang <jchang@Kais-MBP-5.attlocal.net>
2024-07-26 15:04:22 -0500

    matrix exercise

---
+++ problems/matrix/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void initialize(vector<vector<pair<int, int>>>& matrix, int rows, int cols) {
+
+}
+
+void print(const vector<vector<pair<int, int>>>& matrix) {
+
+}
+
+void setPairInGrid(vector<vector<pair<int, int>>>& matrix, int row, int col, int firstValue, int secondValue) {
+
+}
+
+
+int main() {
+
+    vector<vector<pair<int, int>>> matrix;
+
+    initialize(matrix, 5, 5);
+
+    print(matrix);
+
+    setPairInGrid(grid, 1, 1, 5, 10);
+    setPairInGrid(grid, 3, 3, 7, 14);
+
+
+
+}
